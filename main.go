65062f80af8b635f77da359201857ea221b41ef6
fakih <fakih.arief@tokopedia.com>
2016-10-19 22:06:12 +0700

    add some changes in csv and http

--- main.go
+++ main.go
@@ -1,32 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "os"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-    url := "http://i.imgur.com/m1UIjW1.jpg"
 
-    response, e := http.Get(url)
-    if e != nil {
-        log.Fatal(e)
-    }
+	pwd, _ := os.Getwd()
+	urls, _ := getCSVDATA(pwd + "/csv/PRODUCT_DATA_nmax.csv")
+	fmt.Print("total data ")
+	fmt.Println(len(urls))
+	for index := range urls {
 
-    defer response.Body.Close()
+		url := urls[index+1]
+		fmt.Print(index)
+		fmt.Println(" - mendownload " + url)
+		response, e := http.Get(url)
 
-    file, err := os.Create("/tmp/asdf.jpg")
-    if err != nil {
-        log.Fatal(err)
-    }
-    // Use io.Copy to just dump the response body to the file. This supports huge files
-    _, err = io.Copy(file, response.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-    file.Close()
-    fmt.Println("Success!")
+		if e != nil {
+			log.Fatal(e)
+			fmt.Println("gagal download " + url)
+		}
+
+		defer response.Body.Close()
+
+		s := strings.Split(url, "/")
+
+		file, err := os.Create(pwd + "/img/" + s[9])
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Use io.Copy to just dump the response body to the file. This supports huge files
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file.Close()
+		fmt.Println("Success!")
+	}
+}
+
+func getCSVDATA(file string) ([101]string, error) {
+
+	f, _ := os.Open(file)
+	defer f.Close()
+	r := csv.NewReader(bufio.NewReader(f))
+	result, _ := r.ReadAll()
+
+	var urlImage [101]string
+	for i := range result {
+		urlImage[i] = result[i][1]
+	}
+	return urlImage, nil
 }
