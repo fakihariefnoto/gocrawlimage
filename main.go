4c66346f37fed6dee4d43e592c8a42c5d54d7a42
Fakih <fakihariefnoto@gmail.com>
2016-10-19 20:23:51 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    url := "http://i.imgur.com/m1UIjW1.jpg"
+
+    response, e := http.Get(url)
+    if e != nil {
+        log.Fatal(e)
+    }
+
+    defer response.Body.Close()
+
+    file, err := os.Create("/tmp/asdf.jpg")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // Use io.Copy to just dump the response body to the file. This supports huge files
+    _, err = io.Copy(file, response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    file.Close()
+    fmt.Println("Success!")
+}
